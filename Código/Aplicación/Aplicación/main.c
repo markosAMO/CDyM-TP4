508d455e2ba5ad08836b93eab6c37da9f9735633
Markos <marmoscoso1@gmail.com>
2021-08-06 17:55:56 -0300

    Creación del proyecto de atmel

---
+++ Código/Aplicación/Aplicación/main.c
@@ -0,0 +1,18 @@
+/*
+ * Aplicación.c
+ *
+ * Created: 6/08/2021 17:54:52
+ * Author : GOD
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
