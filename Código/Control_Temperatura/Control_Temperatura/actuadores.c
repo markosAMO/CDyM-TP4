20741277766e04f3c9518425836453af91bb5a0b
Agustin Cao <alcaolpg@gmail.com>
2021-08-07 17:44:32 -0300

    actuadores

---
+++ Código/Control_Temperatura/Control_Temperatura/actuadores.c
@@ -0,0 +1,29 @@
+﻿/*
+ * actuadores.c
+ *
+ * Created: 07/08/2021 17:32:45
+ *  Author: Agustín
+ */ 
+
+#include "avr/io.h"
+
+void set_puertos()
+{
+	DDRC = 0x03;
+	PORTC = 0x00;
+}
+
+void encender_estufa()
+{
+	PORTC = 0x01;
+}
+
+void apagar()
+{
+	PORTC = 0x00;
+}
+
+void encender_ventilador()
+{
+	PORTC = 0x02;
+}
