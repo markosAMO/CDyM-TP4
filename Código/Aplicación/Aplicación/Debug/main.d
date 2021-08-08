e2536bc694df107b576fcde1ea24e68204596194
Markos <marmoscoso1@gmail.com>
2021-08-07 21:16:08 -0300

    pruebas
    se probaron cosas

--- Código/Aplicación/Aplicación/Debug/main.d
+++ Código/Aplicación/Aplicación/Debug/main.d
@@ -14,7 +14,8 @@ main.d main.o: .././main.c \
  d:\atmel\ studio\ 7\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\util\delay.h \
  d:\atmel\ studio\ 7\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\util\delay_basic.h \
  d:\atmel\ studio\ 7\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\math.h \
- .././controlador_lcd.h
+ .././controlador_lcd.h \
+ d:\atmel\ studio\ 7\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\interrupt.h
 
 d:\atmel\ studio\ 7\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\io.h:
 
@@ -47,3 +48,5 @@ d:\atmel\ studio\ 7\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\util\delay
 d:\atmel\ studio\ 7\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\math.h:
 
 .././controlador_lcd.h:
+
+d:\atmel\ studio\ 7\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\interrupt.h:
--- Código/Aplicación/Aplicación/main.c
+++ Código/Aplicación/Aplicación/main.c
@@ -9,15 +9,28 @@
 #include "lcd.h"
 #include "controlador_lcd.h"
 #define F_CPU 8000000L
+#include <avr/interrupt.h>
+volatile uint8_t flag = 0;
+volatile uint16_t contador = 0;
 int main(void)
 {
 	
-    LCDinit();
-	actualizarVarInferior("HOLA COMO ESTÁS?");
-	actualizarVarSuperior("HOLA COMO ESTÁS?");
-    while (1) 
+    OCR0A = 250;
+	TCCR0A = 0x02; //se activa modo CTC
+	TCCR0B = 0x02;
+	TIMSK0 = (1<<OCIE0A);
+	sei();
+	while(1)
     {
 		mostrar();
-    }
+	}
+}
+ISR(TIMER0_COMPA_vect){
+	contador++;
+	if(contador == 250) //con esto debería hacer la interrupción cada 0.5 segundos
+	{
+		flag = 1;
+		contador = 0;
+	}
 }
 
